72eff0c13e7ebcac62f196617b93b085d5038543
Fernando Nicolás <fersanti2896@gmail.com>
2023-08-17 02:36:47 -0600

    Creando endpoint para servicio de mostrar imágenes.

--- controller/uploadsController.js
+++ controller/uploadsController.js
@@ -75,10 +75,52 @@ const updateImage = async(req, res = response) => {
 
     res.status(200).json({
         modelo
-    })
+    });
+}
+
+const viewImg = async(req, res = response) => {
+    const { coleccion, id } = req.params;
+
+    let modelo;
+
+    switch ( coleccion ) {
+        case 'usuarios':
+            modelo = await Usuario.findById( id );
+
+            if( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${ id }.`
+                });
+            }
+            break;
+        case 'productos':
+            modelo = await Producto.findById( id );
+    
+            if( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${ id }.`
+                });
+            }
+            break;
+    
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto.' })
+    }
+
+    if( modelo.img ) {
+        const pathImg = path.join( __dirname, '../uploads', coleccion, modelo.img );
+
+        if( fs.existsSync(pathImg) ) {
+            return res.sendFile( pathImg );
+        }
+    }
+
+    const pathImg = path.join( __dirname, '../assets/no-image.jpg' )
+    res.sendFile( pathImg );
 }
 
 module.exports = {
     updateImage,
-    uploadFileServer
+    uploadFileServer,
+    viewImg
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,7 +1,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { uploadFileServer, updateImage } = require('../controller/uploadsController');
+const { uploadFileServer, updateImage, viewImg } = require('../controller/uploadsController');
 const { validarCampos, validarArchivo } = require('../middlewares');
 const { coleccionesPermitidas } = require('../helpers');
 
@@ -15,5 +15,11 @@ router.put('/:coleccion/:id', [
     check('coleccion').custom( c => coleccionesPermitidas(c, ['usuarios', 'productos', 'categorias']) ),
     validarCampos
 ], updateImage);
+
+router.get('/:coleccion/:id', [
+    check('id', 'El id debe ser de Mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas(c, ['usuarios', 'productos', 'categorias']) ),
+    validarCampos
+], viewImg);
 
 module.exports = router;
